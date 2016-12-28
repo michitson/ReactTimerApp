234f8dc02c8b027c6bc179a166561eb0b8364449
Andrew Michitson <michitson@yahoo.com>
2016-12-28 18:26:54 +0000

    initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var Navigation = require('Navigation');
+
+var Main = (props) => {
+    return(
+      <div>
+        <Navigation></Navigation>
+        <div>
+          <div>
+            <p>Main jsx successfully rendered</p>
+            {props.children}
+
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+module.exports = Main;
